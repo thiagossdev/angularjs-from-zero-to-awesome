b6530c718dae0b957ccaa555ad71143999373648
Thiago S. Santos <thiagos.xsantos@gmail.com>
2016-07-01 17:41:01 -0300

    Section 2 - Lesson 3 - Ok

--- section2/lesson3/main.js
+++ section2/lesson3/main.js
@@ -3,9 +3,11 @@ var app = angular.module('codecraft', []);
 app.controller('PersonsController', function ($scope) {
 
 	$scope.selectedIndex = null;
+	$scope.selectedPerson = null;
 
-	$scope.selectPerson = function (index) {
+	$scope.selectPerson = function (person, index) {
 		$scope.selectedIndex = index;
+		$scope.selectedPerson = person
 	};
 
 	$scope.persons = [
